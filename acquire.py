ce9d060e5280f5e8856bcdc5fb6d3bbff074116e
Sophia Stewart <sophia.marcell.stewart@gmail.com>
2022-01-10 15:45:44 -0600

    Removed initial get_data function

--- acquire.py
+++ acquire.py
@@ -2,28 +2,7 @@
 import pandas as pd
 import requests
 
-
 def get_data(target):
-    '''
-    This function takes in a target which specifies the dataset to be retrieved 
-    from https://python.zgulde.net/ It returns a dataframe of that data.
-    '''
-    url = 'https://python.zgulde.net/'
-    p_1 = f'api/v1/{target}'
-    # access url
-    response = requests.get(url+p_1)
-    # assign json data to variable
-    data = response.json()
-    # convert page 1 results to dataframe
-    df = pd.DataFrame(data['payload'][f'{target}'])
-    # retrieve data from all pages
-    while data['payload']['page'] < data['payload']['max_page']:
-        response = requests.get(url + data['payload']['next_page'])
-        data = response.json()
-        df = pd.concat([df, pd.DataFrame(data['payload'][f'{target}'])])
-    return df.reset_index().drop(columns='index')
-
-def get_data_csv(target):
     '''
     This function takes in a target specifying which dataset to retrieve from 
     https://python.zgulde.net/ It returns a dataframe of that data and creates a
